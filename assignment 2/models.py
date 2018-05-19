537491be4114b404f5d1f17b0b17166a8f8db52e
Kaleidophon <Dennis.Ulmer@gmx.de>
2018-05-19 18:53:16 +0200

    Pushing for questions.

--- assignment 2/models.py
+++ assignment 2/models.py
@@ -4,7 +4,10 @@ Main module defining the translation model.
 
 # EXT
 import numpy as np
+import torch
 import torch.nn as nn
+from torch.autograd import Variable
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 
 class AttentionModel(nn.Module):
@@ -35,34 +38,109 @@ class AttentionModel(nn.Module):
         self.dropout = dropout
 
         # Define weights
-        self.word_embeddings = nn.Embedding(self.source_vocab_size, embedding_dim)
+        self.word_embeddings_in = nn.Embedding(self.source_vocab_size, embedding_dim)
+        self.word_embeddings_out = nn.Embedding(self.target_vocab_size, embedding_dim)
         self.positional_embeddings = nn.Embedding(encoded_positions, embedding_dim)
-        self.attention_layer = nn.Linear(embedding_dim * 2, 1)
+        self.attention_layer = nn.Linear(embedding_dim * 2 + hidden_dim, 1)
+        self.attention_soft = nn.Softmax(dim=2)
         # Projecting the context vector (which is the weighted average over concats of positional and word embeddings)
         # concatenated with the current hidden unit to the target vocabulary in order to apply softmax
         self.projection_layer = nn.Linear(2 * embedding_dim + hidden_dim, target_vocab_size)
         # Hidden units on decoder side
-        self.hidden_layer = nn.RNN(input_size=embedding_dim, hidden_size=hidden_dim, dropout=self.dropout)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True, dropout=dropout)
+        self.scale_h0 = nn.Linear(embedding_dim * 2, hidden_dim)
 
-    def forward(self, *input):
+    def forward(self, source_sentences, positions):
         """
         Forward pass through the model.
         """
-        # TODO: Implement
-        ...
+        # Encoder side
+        in_words = self.word_embeddings_in(source_sentences)
+        positions = self.pos_embeddings(positions)
+        combined_embeddings = self.combine_pos_and_word_embedding(in_words, positions)
 
-    def attention(self, *input):
-        """
-        Attention mechanism.
-        """
-        # TODO: Implement dot product first, then with self.attention_layer
-        ...
+        # Decoder side
+        out_words = self.word_embeddings_out(target_sentences)
 
-    def combine_pos_and_word_embedding(self, word_embedding, pos_embedding):
+        # avg out encoder data to use as hidden state
+        avg = torch.mean(combined_embeddings, 1)
+        hidden = self.scale_h0(avg)
+
+        # the hidden state from encoder RNN
+        packed_input = pack_padded_sequence(out_words, target_lengths, batch_first=True)
+        packed_output, (ht, ct) = self.lstm(packed_input, (torch.unsqueeze(hidden, 0), torch.unsqueeze(hidden, 0)))
+        lstm_out, _ = pad_packed_sequence(packed_output, batch_first=True)
+
+        # prepare lstm Hidden layers for input into attention,
+        # we add the hidden layer as zeroth lstm_out and remove the last one
+        # this is because we need h_i-1, not h_i
+        lstm_to_attn = torch.cat((torch.unsqueeze(hidden, 1), lstm_out), 1)
+        lstm_to_attn = lstm_to_attn[:, :-1, :]
+
+        context = self.attention(lstm_to_attn, combined_embeddings, source_lengths, max_len)
+
+        # combine with non existing context vectors
+        combined = torch.cat((lstm_out, lstm_out), 2)
+        out = self.out(combined)
+        return out
+
+    def attention(self, lstm_to_attn, encoder_outputs, source_lengths, max_len):
+        """
+        Defining the Attention mechanism.
+        """
+        # repeat the lstm out in third dimension and the encoder outputs in second dimension so we can make a meshgrid
+        # so we can do elementwise mul for all possible combinations of h_j and s_i
+        h_j = encoder_outputs.unsqueeze(1).repeat(1, lstm_to_attn.size(1), 1, 1)
+        s_i = lstm_to_attn.unsqueeze(2).repeat(1, 1, encoder_outputs.size(1), 1)
+
+        # get the dot product between the two to get the energy
+        # the unsqueezes are there to emulate transposing. so we can use matmul as torch.dot doesnt accept matrices
+        energy = s_i.unsqueeze(3).matmul(h_j.unsqueeze(4)).squeeze(4)
+
+        #         # this is concat attention, its a different form then the ones we need
+        #         cat = torch.cat((s_i,h_j),3)
+
+        #         energy = self.attn_layer(cat)
+
+        # reshaping the encoder outputs for later
+        encoder_outputs = encoder_outputs.unsqueeze(1)
+        encoder_outputs = encoder_outputs.repeat(1, energy.size(1), 1, 1)
+
+        # apply softmax to the energys
+        alignment = self.attn_soft(energy)
+
+        # create a mask like : [1,1,1,0,0,0] whos goal is to multiply the attentions of the pads with 0, rest with 1
+        idxes = torch.arange(0, max_len, out=max_len).unsqueeze(0)
+        mask = Variable((idxes < source_lengths.unsqueeze(1)).float())
+
+        # format the mask to be same size() as the attentions
+        mask = mask.unsqueeze(1).unsqueeze(3).repeat(1, alignment.size(1), 1, 1)
+
+        # apply mask
+        masked = alignment * mask
+
+        # now we have to rebalance the other values so they sum to 1 again
+        # this is done by dividing each value by the sum of the sequence
+        # calculate sums
+        msum = masked.sum(-2).repeat(1, 1, masked.size(2)).unsqueeze(3)
+
+        # rebalance
+        alignment = masked.div(msum)
+
+        # now we shape the attentions to be similar to context in size
+        alignment = alignment.repeat(1, 1, 1, encoder_outputs.size(3))
+
+        # make context vector by element wise mul
+        context = alignment * encoder_outputs
+
+        return context
+
+    @staticmethod
+    def combine_pos_and_word_embedding(words, positions):
         """
         Define the way positional and word embeddings are combined on the "encoder" side.
         """
-        return self.embedding_comb_func(word_embedding, pos_embedding)
+        return torch.cat((words, positions), 2)
 
     def load(self, model_path):
         # TODO: Implement
--- assignment 2/train.py
+++ assignment 2/train.py
@@ -6,3 +6,63 @@ Module defining the model training function.
 def train():
     # TODO: Implement
     ...
+
+
+epoch_losses = []
+iterations = len(data_loader)
+
+for epoch in range(0, num_epochs):
+    start = time.time()
+    batch_losses = []
+    batch = 0
+    for source_batch, target_batch, source_lengths, target_lengths, batch_positions in data_loader:
+        batch_start = time.time()
+        max_len = source_lengths.max()
+        source_batch = source_batch[:, :max_len]
+        batch_positions = batch_positions[:, :max_len]
+
+        source_batch = torch.autograd.Variable(source_batch)
+        target_batch = torch.autograd.Variable(target_batch)
+        source_lengths = torch.autograd.Variable(source_lengths)
+        target_lengths = torch.autograd.Variable(target_lengths)
+        batch_positions = torch.autograd.Variable(batch_positions)
+
+        if (cuda):
+            source_batch = source_batch.cuda()
+            target_batch = target_batch.cuda()
+            source_lengths = source_lengths.cuda()
+            target_lengths = target_lengths.cuda()
+            batch_positions = batch_positions.cuda()
+
+        # get encoder and decoder outputs
+        encoder_out = encoder(source_batch, batch_positions)
+        decoder_out = decoder(target_batch, target_lengths, source_lengths, encoder_out, max_len)
+
+        # remove the start token from the targets and the end token from the decoder
+        decoder_out2 = decoder_out[:, :-1, :]
+        target_batch2 = target_batch[:, 1:decoder_out.size(1)]
+
+        # pytorch expects the inputs for the loss function to be: (batch x classes)
+        # cant handle sentences so we just transform our data to treath each individual word as a batch:
+        # so new batch size = old batch * padded sentence length
+        # should not matter as it all gets averaged out anyway
+        decoder_out3 = decoder_out2.contiguous().view(decoder_out2.size(0) * decoder_out2.size(1), decoder_out2.size(2))
+        target_batch3 = target_batch2.contiguous().view(target_batch2.size(0) * target_batch2.size(1))
+
+        # calculate loss
+        loss = loss_function(decoder_out3, target_batch3)
+        batch_losses.append(loss)
+
+        # backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        batch_time = time.time() - batch_start
+        print('\r[Epoch {:03d}/{:03d}] Batch {:06d}/{:06d} [{:.1f}/s] '.format(epoch + 1, num_epochs, batch + 1,
+                                                                               iterations, batch_time), end='')
+        batch += 1
+
+    avg_loss = sum(batch_losses) / iterations
+    epoch_losses.append(avg_loss)
+    print('Time: {:.1f}s Loss: {:.3f} score2?: {:.6f}'.format(time.time() - start, avg_loss, 0))
