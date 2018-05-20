fe9c4e780a12478adf35697c98233005ffcc6683
Kaleidophon <Dennis.Ulmer@gmx.de>
2018-05-21 00:11:09 +0200

    Making evaluation with BLEU somewhat easier.

--- assignment 2/eval.py
+++ assignment 2/eval.py
@@ -4,8 +4,14 @@ Module defining all the evaluation functions for the model.
 
 # STD
 import codecs
+import subprocess
+import sys
+import os
+from io import StringIO
+import time
 
 # EXT
+import numpy as np
 from torch.utils.data import DataLoader
 import torch.nn as nn
 
@@ -14,11 +20,11 @@ from models import AttentionModel
 from corpus import ParallelCorpus
 
 
-def evaluate(model, eval_set, target_path):
+def evaluate(model, eval_set, target_path, reference_file_path):
     data_loader = DataLoader(eval_set, batch_size=5)
     softmax = nn.Softmax(dim=2)
     idx2word = evaluation_set.target_i2w
-    sorted_sentence_ids = evaluation_set.target_sentence_ids
+    sorted_sentence_ids = evaluation_set.target_sentence_ids.numpy()
     translated_sentences = []
 
     # Decode
@@ -35,27 +41,31 @@ def evaluate(model, eval_set, target_path):
             eos_index = len(tokens)
             if "<eos>" in tokens:
                 eos_index = tokens.index("<eos>")
+
             tokens = tokens[:eos_index]  # Cut off after first end of sentence token
-            translated_sentence = " ".join(tokens)
+            translated_sentence = " ".join(tokens).replace("@@ ", "")
             translated_sentences.append(translated_sentence)
-            print(translated_sentence)
 
     # Bring sentence back into the order they were in the test set
-    # TODO
+    translated_sentences = np.array(translated_sentences)[sorted_sentence_ids]
 
     # Write to file
     with codecs.open(target_path, "wb", "utf-8") as target_file:
         for sentence in translated_sentences:
             target_file.write("{}\n".format(sentence))
 
-    # In terminal: Apply moses script for BLEU
-    # https://github.com/moses-smt/mosesdecoder/blob/master/scripts/generic/multi-bleu.perl
+    out = subprocess.getoutput(
+        "perl ./multi-bleu.perl {} < {}".format(reference_file_path, target_path)
+    )
+    print(out[out.index("BLEU"):])
+
+    # TODO
     # In terminal: Download and apply METEOR
     # http://www.cs.cmu.edu/~alavie/METEOR/
 
 
 if __name__ == "__main__":
-    model = AttentionModel.load("./attentionmodel_epoch2.model")
+    model = AttentionModel.load("./attentionmodel_epoch30.model")
     max_allowed_sentence_len = 50
     training_set = ParallelCorpus(
         source_path="./data/train/train_bpe.fr", target_path="./data/train/train_bpe.en",
@@ -65,4 +75,7 @@ if __name__ == "__main__":
         source_path="./data/test/test_2017_flickr_bpe.fr", target_path="./data/test/test_2017_flickr_bpe.en",
         max_sentence_length=max_allowed_sentence_len, use_indices_from=training_set
     )
-    evaluate(model, evaluation_set, target_path="./eval_out.txt")
+    evaluate(
+        model, evaluation_set, target_path="./eval_out.txt",
+        reference_file_path="./data/test/test_2017_flickr_truecased.en"
+    )
